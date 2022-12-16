ea11043384a6e31a4e11ce24f9e53a95b57ab40f
siva-ranjani-r <siva2300rs@gmail.com>
2022-12-16 17:43:03 +0530

    Adding js file.

---
+++ js/new.js
@@ -0,0 +1,15 @@
+const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const d = new Date();
+let day = weekday[d.getDay()];
+console.log(day);
+
+
+
+const x=new Date(); 
+a=x.getDay();   
+console.log(a);
+
+
+const x=new Date(1999); 
+b=x.getYear();   
+console.log(b);
